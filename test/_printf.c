416897ec71346648f76b1c01700198faeb489bce
mdjibo2 <justpourmestest00@gmail.com>
2022-08-07 17:49:45 -0700

    Print string with identifier %s

--- test/_printf.c
+++ test/_printf.c
@@ -9,13 +9,34 @@ int _printf(const char *format, ...)
 {
 	int a = 0;
 	int b = 0;
+	va_list args;
+
+	va_start(args, format);
 
 	while (format[a])
 	{
-		_putchar(format[a]);
-		b++;
+		while (format[a] == '%')
+		{
+			switch (format[a + 1])
+					{
+					case 's':
+						b += _printStr(args);
+						a += 2;
+						break;
+					default:
+						_putchar(format[a]); 
+						_putchar(format[a + 1]);
+						a += 2;
+					}
+		}
+		
+		if (format[a])
+		{
+			_putchar(format[a]);
+			b++;
+		}
 		a++;
 	}
-
+	va_end(args);
 	return (b);
 }
--- test/main.h
+++ test/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include<stdarg.h>
+
 int _putchar(char c);
 int _printf(const char *format, ...);
+int _printStr(va_list);
 
 #endif
