a93c8b4985a638a3203fceaf9f1641716ace04ef
mdjibo2 <justpourmestest00@gmail.com>
2022-08-09 02:28:40 -0700

    from test to repo home

--- _printf.c
+++ _printf.c
@@ -3,19 +3,50 @@
 /**
  * _printf - prints anything
  * @format: the format string
- * Return: the number of characters printed
+ * Return; returns the number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
 	int a = 0;
 	int b = 0;
+	va_list args;
 
-	if (format !='\0')
+	va_start(args, format);
+
+	while (format[a])
 	{
-		_putchar(format[a]);
+		while (format[a] == '%')
+		{
+			switch(format[a + 1])
+			{
+					case 'c':
+						b += _printChar(args);
+						a += 2;
+						break;
+					case 's':
+						b += _printStr(args);
+						a += 2;
+						break;
+					case '%':
+						_putchar('%');
+						b++;
+						a += 2;
+						break;
+					default:
+						_putchar(format[a]);
+						_putchar(format[a + 1]);
+						a +=2;
+			}
+		}
+		
+		if (format[a])
+		{
+			_putchar(format[a]);
+			b++;
+		}
 		a++;
-		b++;
 	}
-
+	va_end(args);
 	return (b);
 }
