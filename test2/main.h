00fc06b4f4469dad47e6889c6afe5adc468d5e63
mdjibo2 <justpourmestest00@gmail.com>
2022-08-09 07:22:13 -0700

    Correction

--- test2/main.h
+++ test2/main.h
@@ -8,11 +8,8 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int _printChar(va_list);
 int _printStr(va_list);
-<<<<<<< HEAD:test2/main.h
 int _printInt(va_list args);
-=======
 int _print_dec(va_list args);
 int _print_int(va_list args);
->>>>>>> 901b577dc4c5ca14e43aa36d83aa658e8dba9268:test/main.h
 
 #endif
